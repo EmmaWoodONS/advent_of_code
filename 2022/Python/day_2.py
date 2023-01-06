95400172175bcdb50be7931c092495915b47581e
Wood <emma.wood@ons.gov.uk>
2023-01-06 09:00:31 +0000

    get part a answer

--- 2022/Python/day_2.py
+++ 2022/Python/day_2.py
@@ -15,9 +15,9 @@ data_d2 = pd.read_csv('data/day_2.csv', header = None)
 data_d2.columns = ["theirs", "mine"]
 
 combos = np.array([(x, y) for x in ("A", "B", "C") for y in (["X", 1], ["Y", 2], ["Z", 3])], dtype = "object")
-combos_df = pd.DataFrame(combos, columns = ['them','me'])
-type_scores = pd.DataFrame(combos_df['me'].tolist(),\
-     columns = ['me', 'type_score']).join(combos_df["them"])
+combos_df = pd.DataFrame(combos, columns = ["them", "me"])
+type_scores = pd.DataFrame(combos_df["me"].tolist(),\
+     columns = ["me", "type_score"]).join(combos_df["them"])
 
 win = pd.DataFrame(np.array([["A", "Y", 6], ["B", "Z", 6], ["C", "X", 6]]), columns = ["them", "me", "result_score"])
 lose = pd.DataFrame(np.array([["A", "Z", 0], ["B", "X", 0], ["C", "Y", 0]]), columns = ["them", "me", "result_score"])
@@ -25,3 +25,6 @@ draw = pd.DataFrame(np.array([["A", "X", 3], ["B", "Y", 3], ["C", "Z", 3]]), col
 result_scores = pd.concat([win, lose, draw])
 
 scores_joined = type_scores.merge(result_scores) 
+scores_joined["final_score"] = pd.to_numeric(scores_joined["result_score"]) + pd.to_numeric(scores_joined["type_score"])
+
+score = scores_joined["final_score"].sum()
